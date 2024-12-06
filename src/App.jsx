ca30eadf196c700551b828ae6868a438ab42169f
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-12-06 12:06:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,11 @@ import Login from "./components/Login";
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        {/* Navigation Buttons */}
-        <div className="p-4 space-x-4">
-          <Link to="/" className="bg-blue-500 text-white px-4 py-2 rounded">
-            Go to Login Page
-          </Link>
-          <Link to="/users" className="bg-blue-500 text-white px-4 py-2 rounded">
-            Go to Users List
-          </Link>
-        </div>
-
-        {/* Routes */}
+      
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/users" element={<UsersList />} />
         </Routes>
-      </div>
     </BrowserRouter>
   );
 };
